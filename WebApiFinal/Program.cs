726730e35634defdc0350adae2d1926097b76e18
GUIlhermegomes1203 <guigomessouza20002019@gmail.com>
2025-05-26 08:55:15 -0300

    Adicionar arquivos de projeto.

---
+++ WebApiFinal/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApiFinal.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebApiFinalContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebApiFinalContext") ?? throw new InvalidOperationException("Connection string 'WebApiFinalContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
